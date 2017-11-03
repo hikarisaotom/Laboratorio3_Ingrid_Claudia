9d07545aa9b485d9391aae462d63e6b71354e5cf
dominguezi10 <dominguezi10@unitec.edu>
2017-11-03 13:41:41 -0600

    Se crearon la clase guerrero y sus hijas

---
+++ Lab4_Ingrid_Claudia/src/lab4_ingrid_claudia/Dragon.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4_ingrid_claudia;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author 1234
+ */
+public class Dragon extends Guerrero{
+    private Color Color;
+    private String Raza;
+
+    public Dragon() {
+        super();
+    }
+
+    public Dragon(Color Color, String Raza, String Nombre, int Edad, String Lugar_Nacimiento, int Poder_Ataque, int Salud, int Costo) {
+        super(Nombre, Edad, Lugar_Nacimiento, Poder_Ataque, Salud, Costo);
+        this.Color = Color;
+        this.Raza = Raza;
+    }
+
+    public Color getColor() {
+        return Color;
+    }
+
+    public void setColor(Color Color) {
+        this.Color = Color;
+    }
+
+    public String getRaza() {
+        return Raza;
+    }
+
+    public void setRaza(String Raza) {
+        this.Raza = Raza;
+    }
+
+    @Override
+    public String toString() {
+        return "Dragon{" + "Color=" + Color + ", Raza=" + Raza + '}';
+    }
+    
+    public void Ataque(){
+        
+    }
+    
+}
---
+++ Lab4_Ingrid_Claudia/src/lab4_ingrid_claudia/Mago.java
@@ -0,0 +1,47 @@
+
+package lab4_ingrid_claudia;
+
+/**
+ *
+ * @author 1234
+ */
+public class Mago extends Guerrero{
+    private String Tipo_Magia;
+    private String Elemento_Ataque;
+
+    public Mago() {
+        super();
+    }
+
+    public Mago(String Tipo_Magia, String Elemento_Ataque, String Nombre, int Edad, String Lugar_Nacimiento, int Poder_Ataque, int Salud, int Costo) {
+        super(Nombre, Edad, Lugar_Nacimiento, Poder_Ataque, Salud, Costo);
+        this.Tipo_Magia = Tipo_Magia;
+        this.Elemento_Ataque = Elemento_Ataque;
+    }
+
+    public String getTipo_Magia() {
+        return Tipo_Magia;
+    }
+
+    public void setTipo_Magia(String Tipo_Magia) {
+        this.Tipo_Magia = Tipo_Magia;
+    }
+
+    public String getElemento_Ataque() {
+        return Elemento_Ataque;
+    }
+
+    public void setElemento_Ataque(String Elemento_Ataque) {
+        this.Elemento_Ataque = Elemento_Ataque;
+    }
+
+    @Override
+    public String toString() {
+        return "Mago{" + "Tipo_Magia=" + Tipo_Magia + ", Elemento_Ataque=" + Elemento_Ataque + '}';
+    }
+  
+    
+    public void Ataque(){
+        
+    }
+}
